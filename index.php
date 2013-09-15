e41e33eaa0831bfa8701f46e2704d94888737d95
Edward Chik <edward.chik@gmail.com>
2013-09-15 11:51:08 -0400

    Added initial index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CSCI E-15 - Project 1</title>
+</head>
+
+<body>
+<p>This is a temporary page for CSCI E-15 project 1.</p>
+</body>
+
+</html>
