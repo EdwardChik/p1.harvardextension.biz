b73db58d0796d5bdcaf1b9c8368c09925e1e7233
Edward Chik <edward.chik@gmail.com>
2013-09-16 00:05:16 -0400

    Added information and external cascading style sheet

--- index.php
+++ index.php
@@ -1,12 +1,71 @@
 <!doctype html>
 <html>
 <head>
+	<!-- character encoding -->
 	<meta charset="utf-8">
 	<title>CSCI E-15 - Project 1</title>
+
+	<!-- external style sheet for formatting -->
+	<link rel="stylesheet" type="text/css" href="default.css">
 </head>
 
 <body>
-<p>This is a temporary page for CSCI E-15 project 1!!!</p>
+	<!-- header section -->
+	<header>
+		<h1>CSCI E-15 - Project 1</h1>
+	</header>
+
+	<!-- site navigation -->
+	<nav>
+		<ul>
+			<li><a href="http://p1.harvardextension.biz">Assignment 1</a> |</li>
+			<li>Assignment 2 (coming soon) |</li>
+			<li>Assignment 3 (coming soon) |</li>
+			<li>Assignment 4 (coming soon)</li>
+		</ul>
+	</nav>
+
+	<!-- main page content -->
+	<article>
+		<!-- Name and Biography -->
+		<section>
+			<h2>About Me</h2>
+
+			<p>Hello there! My name is Edward Chik, and I am currently an aspiring student with the Harvard Extension School who is hoping to enter the Masters of Information Technology program with a focus on <a href="http://www.extension.harvard.edu/degrees-certificates/information-technology/degree-requirements/concentrations#digital" target="_blank">Digital Media and Instructional Design</a>. My hope is that this will combine with my bachelor's degree in communications to form a strong educational foundation for my career in digital media.</p>
+
+			<p>On a personal level, I was born and raised in Toronto, Ontario, Canada where I still currently live. When I have some spare time, I enjoy spending it on hobbies such as video games, reading, writing, attending concerts and following sports (especially the business side, which has always been fascinating to me).</p>
+		</section>
+
+		<!-- Code Editor for CSCI E-15 -->
+		<section>
+			<h2>Code Editor</h2>
+
+			<p>The code editor that I have chosen to use for CSCI E-15 is <a href="http://www.sublimetext.com" target="_blank">Sublime Text 2</a>, a small but very powerful editor that I was recently introduced to. After evaluating other options such as Notepad++ (which I use at my day job) and TextWrangler (which I used a little bit during Harvard Summer School), I decided to use Sublime Text 2 due to a combination of its preference in my other fall class (DGMD E-20) as well as a recommendation of the product by a co-worker who uses it regularly. I believe it offers me the right balance between power and ease of comprehension, I wanted a tool that was more sophisticated than Notepad but not as complex as a high level integrated development environment such as Eclipse (which is another popular option among programmers that I know).</p>
+		</section>
+
+		<!-- Validation of HTML and CSS -->
+		<section>
+			<h2>Validation of HTML and CSS</h2>
+
+			<p>I take proper validation of my HTML and CSS code very seriously! To save anyone reading this page from the hassle of looking up either this page or the corresponding stylesheet separately and plugging it into the W3 Validator tool, I have taken the liberty of putting it together for you:</p>
+
+			<ul>
+				<li></li>
+				<li></li>
+			</ul>
+		</section>
+	</article>
+
+	<!-- footer section -->
+	<footer>
+		<!-- address -->
+		<address>
+			<p>Address: <a href="https://www.google.com/maps/preview#!q=51+Brattle+Street%2C+Cambridge%2C+MA" target="_blank">51 Brattle Street, Cambridge, MA, 02138, USA</a></p>
+		</address>
+
+		<!-- my copyright -->
+		<p>Copyright of Edward Chik, 2013</p>
+	</footer>
 </body>
 
 </html>
